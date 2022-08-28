ee1cbb04e47e18f7ad89c35737f833d3997e1735
Yojan Gautam <gautamyojan0@gmail.com>
2022-08-27 22:15:30 -0400

    new

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ const searchInput = document.querySelector("#searchbar > input")
 const searchButton = document.querySelector("#searchbar > button")
 
 const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/"}
-const engine = "google"
+const engine = "duckduckgo"
 const engineUrls = {
   deepl: "https://www.deepl.com/translator#-/-/",
   duckduckgo: "https://duckduckgo.com/?q=",
@@ -47,11 +47,7 @@ searchButton.onclick = search
  * inject bookmarks into html
  */
 
-const bookmarks = [{"id":"daqKlBnhhjeQCvJX","label":"school","bookmarks":[{"id":"Oza1iNNHoNGo3QZq","label":"Blackboard","url":"https://mymasonportal.gmu.edu/ultra"},{"id":"PoLe5rz8YErw2QPG","label":"zyBooks","url":"https://learn.zybooks.com/library"}]},{"id":"Vxr8rKdq6on2iyQy","label":"personal","bookmarks":[{"id":"mCz9L89h9DGP4v5G","label":"Calendar","url":"calendar.google.com"},{"id":"XMsKX5JJR0rwsoxN","label":"Github","url":"https://github.com"},{"id":"9PbrUjzZfa83KlHZ","label":"Drive","url":"https://drive.google.com"},{"id":"78SroxrofoPF5YMj","label":"Hack The Box","url":"https://www.hackthebox.com/"}]},{"id":"sBdemKBwvdTHWBhc","label":"media","bookmarks":[{"id":"IjfglOkv6KaqBoHa","label":"Youtube","url":"https://youtube.com"},{"id":"XbAPuBvnheCP9EHV","label":"Twitch","url":"https://twitch.tv"},{"id":"dXumV8b9Qc7MsYsE","label":"Reddit","url":"https://reddit.com"}]},{"id":"fmTqZvKywsIoliSS","label":"misc","bookmarks":[{"id":"gKDxFUQrPaZoyMBY","label":"Youtube Studio","url":"https://studio.youtube.com"},{"id":"JywcEGWMOVSmPXqH","label":"gif","url":"https://designyoutrust.com/2019/05/the-chill-and-retro-motion-pixel-art-of-motocross-saito/"},{"id":"OJXj3ywKaG4OiaPy","label":"@startpage","url":"https://prettycoffee.github.io/startpage"},{"id":"gxhJhjfO3nDk9NVm","label":"author","url":"https://prettycoffee.github.io/"}]}]
-
-
-
-// const bookmarks = [{"id":"MkF5AnK7lFN9SVE0","label":"college","bookmarks":[{"id":"EhIT1yYHNZUJ5n1B","label":"Blackboard","url":"https://mymasonportal.gmu.edu/"},{"id":"fs4UJMHWucbegfJ0","label":"zyBooks","url":"https://learn.zybooks.com/signin"},{"id":"E2usC74RLieWEOJH","label":"Outlook","url":"https://outlook.office.com/mail/?actSwt=true"},{"id":"DxtOvZEtdCQuD1Xa","label":"OneDrive","url":"https://gmuedu-my.sharepoint.com/personal/ygautam2_gmu_edu/_layouts/15/onedrive.aspx"}]},{"id":"O3F5PfEolVqCiPtr","label":"personal","bookmarks":[{"id":"iEK9LzvYNVgohrPH","label":"Calendar","url":"https://calendar.google.com/calendar/u/0/r"},{"id":"eRc6EoohMcVirkGx","label":"Github","url":"https://www.github.com"},{"id":"eRc6EoohMcVirkGz","label":"Drive","url":"https://drive.google.com/drive/u/0/my-drive"}]},{"id":"DqAMQIq2wIVAKUF2","label":"media","bookmarks":[{"id":"FnHM9TVSLqICrgaW","label":"Youtube","url":"https://youtube.com"},{"id":"4SJCIQkFTV57t116","label":"Twitch","url":"https://twitch.tv"}]}]
+const bookmarks = [{"id":"ybJrFH35FmdI0dOc","label":"college","bookmarks":[{"id":"vuljUCL4UQ9HuZ2J","label":"pearson","url":"https://portal.mypearson.com"},{"id":"imShpXZboXqLF39K","label":"blackboard","url":"https://mymasonportal.gmu.edu"},{"id":"eKPGRPmaTcyDzb6f","label":"piazza","url":"https://piazza.com/class/l6mwp0wb7b57eq"}]},{"id":"3HM2nYTbdZB1kMXp","label":"personal","bookmarks":[{"id":"eKpfFbNAcXC8YEKJ","label":"calendar","url":"https://calendar.google.com/calendar"},{"id":"uPORlgoCHNKJMoxk","label":"github","url":"https://github.com"},{"id":"CaWzmdttITSGGR1C","label":"drive","url":"https://drive.google.com"}]},{"id":"GNOOfV8rRLmN6lPI","label":"media","bookmarks":[{"id":"VPAowMC2dsqY0vQ3","label":"youtube","url":"https://www.youtube.com"},{"id":"82TRQKHy9Uu3rUmh","label":"twitch","url":"https://twitch.com"},{"id":"gMOT2wHDfyMzMzuy","label":"reddit","url":"https://reddit.com"}]},{"id":"mcVg4fL2hZja0yW7","label":"misc","bookmarks":[{"id":"nD2swQlN9cR5TMhv","label":"studio","url":"https://studio.youtube.com"},{"id":"5Gqj1OX6fMpsOXvN","label":"mail","url":"https://outlook.office.com/mail/?actSwt=true"}]}]
 
 const createGroupContainer = () => {
   const container = document.createElement("div")
